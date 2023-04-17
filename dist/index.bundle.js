00e8999592822ca7d39229321121a0dec31575e5
Hernan Zamora <hernanzamora1002@gmail.com>
2023-04-17 12:55:12 -0300

    Setted up project with webpack - linterns - jest

---
+++ dist/index.bundle.js
@@ -0,0 +1,13 @@
+(self["webpackChunkjavascript_capstone"] = self["webpackChunkjavascript_capstone"] || []).push([[0],[
+/* 0 */
+/***/ (() => {
+
+
+
+/***/ })
+],
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__(0));
+/******/ }
+]);
