046382b15c91a231b484f46820e9aed27b4f6017
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-04-19 20:06:12 +0500

    Correct cards data rendering in modal

--- src/modules/displayReservations.js
+++ src/modules/displayReservations.js
@@ -3,17 +3,20 @@ const BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capston
 
 /* Getting reservations list from the Involvement API */
 const DisplayReservations = async () => {
-  const openModalBtn = document.querySelectorAll('[data-modal-target]');
-  openModalBtn.forEach((btn) => {
-    btn.addEventListener('click', async () => {
-      const res = await fetch(`${BASE_URL}?item_id=${btn.id}`);
-      const data = await res.json();
-      const div = document.getElementById('reservations-list');
-      div.innerHTML = '';
-      data.forEach((reservation) => {
-        const li = document.createElement('li');
-        li.innerHTML = `${reservation.date_start} - ${reservation.date_end} by ${reservation.username}`;
-        div.appendChild(li);
+  const cardContainers = document.querySelectorAll('.card');
+  cardContainers.forEach((card) => {
+    const openModalBtn = card.querySelectorAll('[data-modal-target]');
+    openModalBtn.forEach((btn) => {
+      btn.addEventListener('click', async () => {
+        const res = await fetch(`${BASE_URL}?item_id=${btn.id}`);
+        const data = await res.json();
+        const div = card.querySelector('.reservations-list');
+        div.innerHTML = '';
+        data.forEach((reservation) => {
+          const li = document.createElement('li');
+          li.innerHTML = `${reservation.date_start} - ${reservation.date_end} by ${reservation.username}`;
+          div.appendChild(li);
+        });
       });
     });
   });
