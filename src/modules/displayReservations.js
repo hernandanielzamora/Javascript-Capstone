0e54012a71dc72d09eb16e385c4fc6b55162bbe3
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-04-20 00:39:03 +0500

    Change made as per the reviewer suggestions

--- src/modules/displayReservations.js
+++ src/modules/displayReservations.js
@@ -7,23 +7,32 @@ const BASE_URL = 'https://us-central1-involvement-api.cloudfunctions.net/capston
 const DisplayReservations = async () => {
   const cardContainers = document.querySelectorAll('.card');
   cardContainers.forEach((card) => {
+    const reservationsTitle = card.querySelector('.reservation-title');
     const openModalBtn = card.querySelectorAll('[data-modal-target]');
     openModalBtn.forEach((btn) => {
       btn.addEventListener('click', async () => {
-        const res = await fetch(`${BASE_URL}?item_id=${btn.id}`);
-        const data = await res.json();
-        const reservationsCounter = data.length;
-        const div = card.querySelector('.reservations-list');
-        div.innerHTML = '';
-        data.forEach((reservation) => {
-          const li = document.createElement('li');
-          li.innerHTML = `${reservation.date_start} - ${reservation.date_end} by ${reservation.username}`;
-          div.appendChild(li);
-        });
-        const reservationsTitle = document.querySelectorAll('.reservation-title');
-        reservationsTitle.forEach((title) => {
-          ReservationsCounter(reservationsCounter, title);
-        });
+        try {
+          const res = await fetch(`${BASE_URL}?item_id=${btn.id}`);
+          const data = await res.json();
+          const reservationsCounter = data.length;
+          const div = card.querySelector('.reservations-list');
+          div.innerHTML = '';
+          data.forEach((reservation) => {
+            const li = document.createElement('li');
+            li.innerHTML = `${reservation.date_start} - ${reservation.date_end} por ${reservation.username}`;
+            div.appendChild(li);
+          });
+          if (reservationsCounter > 0) {
+            ReservationsCounter(reservationsCounter, reservationsTitle);
+          } else {
+            reservationsTitle.innerHTML = 'Reservations (0)';
+          }
+        } catch (error) {
+          const errorMessage = 'Error, try again later.';
+          const errorElement = document.createElement('div');
+          errorElement.innerText = errorMessage;
+          document.body.appendChild(errorElement);
+        }
       });
     });
   });
