ceafacb85bc5b3b9a720205bca60527d26c1034d
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-04-19 11:55:31 +0500

    Remove conflicting code of dialoge box

--- src/modules/fetchingData.js
+++ src/modules/fetchingData.js
@@ -32,6 +32,27 @@ const fetchPokemons = async () => {
                            <button  data-modal-target="#popup${data.id}" class="comment-btn" id="comment-btn">Comments</button>
                            <button data-modal-target="#modal${data.id}" class="reservation-btn" id="reservation-btn">Reservation</button>
                          </div>
+                         <dialog id='popup${data.id}' class=''>
+                          <button data-close-button id='closeDialoge' class='absolute btn-noBg' type='button' title='closing button'>
+                            <svg xmlns='http://www.w3.org/2000/svg' width='36' height='36' fill='black' class='bi bi-x' viewBox='0 0 16 16'>
+                              <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z'/>
+                            </svg>
+                          </button>
+                          <img src="${data.sprites.front_default}" alt="${data.name}" class="card-img">
+                          <div class="card-header">
+                            <h2>${data.name.toUpperCase()}</h2>
+                          </div>
+                          <div class="specs">
+                            <div class=>
+                              <p>Weight: ${data.weight}</p>
+                              <p>Height: ${data.height}</p>
+                            </div>
+                            <div>
+                              <p>Base experience: ${data.base_experience}</p>
+                              <p>Order: ${data.order}</p>
+                            </div>
+                          </div>
+                        </dialog>
                          <dialog id='modal${data.id}' class='reservation-modal'>
                           <button data-close-button id='closeDialoge' class='absolute btn-noBg' type='button' title='closing button'>
                            <i class="fa-solid fa-xmark fa-2xl"></i>
